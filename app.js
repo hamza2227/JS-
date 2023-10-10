21bc1814b0ae7a537aa5dfef8c4b10395b4e8de1
Hamza Altaf <86216890+hamza2227@users.noreply.github.com>
2023-10-10 22:46:41 +0500

    Js

---
+++ app.js
@@ -0,0 +1,64 @@
+// console.log("Bilal");
+// alert("hamza")
+ 
+// document.getElementById("btn-1").onclick="alert('Clicked')"
+
+// let arr = ["Hamza", "Ali", "Qaisar", "Bilal"];
+// console.log(arr);
+
+// arr[2] = "Adeel";
+// console.log(arr);
+
+// arr.pop(0, 1);
+// console.log(arr);
+
+// arr.push("Arqam");
+// console.log(arr);
+
+// arr.splice(1, 1);
+// console.log(arr);
+
+// arr.sort();
+// console.log(arr);
+
+// arr.reverse();
+// console.log(arr);
+
+const car = {
+    color: 'Red',
+    model: '2014',
+    price: '3M'
+}
+
+console.log(car);
+console.log(car.model);
+console.log(car.price);
+console.log(car.color);
+
+// var convertObjtoString = JSON.stringify(car);
+// console.log(convertObjtoString);
+
+// var convertStringtoObj = JSON.parse(convertObjtoString);
+// console.log(convertStringtoObj);
+
+// var btn_1 = document.getElementById("btn_1");
+// console.log(btn_1.textContent);
+
+// var name;
+
+// function sum(a, b){
+//     return a + b;
+// }
+
+// function sub(a, b){
+//     return a - b;
+// }
+
+// function mul(a, b){
+//     return a * b;
+// }
+
+// function div(a, b){
+//     return a / b;
+// }
+
